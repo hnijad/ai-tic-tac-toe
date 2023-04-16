7c73559fdd1871e4486bc3d0fe9bde20303642ef
DataMonarch <toghrultahirov2@gmail.com>
2023-04-16 23:53:34 +0400

    Added the minmax

--- app/game.py
+++ app/game.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 from typing import List, Tuple
 
@@ -65,24 +66,82 @@ class Board:
             print(row)
             
 class Bot:
+    """
+    Bot class that implements the minimax algorithm.
+    Here, the bot is the minimizer and the player is the maximizer.
     
-    def __init__(self, level: int=0, player: str='O') -> None:
+    
+    """
+    def __init__(self, level: int=1, bot_mark: str='O') -> None:
         self.level = level
-        self.player = player
+        self.bot_mark = bot_mark
     
     def random_move(self, board) -> Tuple[int, int]:
         empty_squares = board.get_empty_squares()
         random_square = empty_squares[np.random.randint(len(empty_squares))]
         return random_square
     
+    def minimax(self, board, is_maximizing, depth=None):
+        # terminal states
+        opponent = 'X' if self.bot_mark == 'O' else 'O'
+        
+        # bot wins              
+        if board.check_win(self.bot_mark):
+            return -1, None # eval, move
+        
+        # player wins
+        if board.check_win(opponent):
+            return 1, None # eval, move
+        
+        # draw 
+        if board.is_board_full():
+            return 0, None # eval, move
+        
+        if is_maximizing:
+            max_score = -float('inf')
+            best_move = None
+            
+            for row, col in board.get_empty_squares():
+                temp_board = copy.deepcopy(board)
+                
+                temp_board.mark_square(row, col, opponent)
+                score, _ = self.minimax(temp_board, False)
+                
+                if score > max_score:
+                    max_score = score
+                    best_move = (row, col)
+                    
+            return max_score, best_move
+        
+        else:
+            min_score = float('inf')
+            best_move = None
+            
+            for row, col in board.get_empty_squares():
+                temp_board = copy.deepcopy(board)
+                
+                temp_board.mark_square(row, col, self.bot_mark)
+                score, _ = self.minimax(temp_board, True)
+                
+                if score < min_score:
+                    min_score = score
+                    best_move = (row, col)
+                    
+            return min_score, best_move
+            
+        
+    
     def eval(self, board):
         if self.level == 0:
             # make a random move
+            score = 'random'
             move = self.random_move(board)
         
         else:
             # make a move based on the heuristic and minimax algorithm
-            pass
+            score, move = self.minimax(board, 0, True)
+        
+        print(f"Bot's move: {move} with score {score}")
         
         return move
     
