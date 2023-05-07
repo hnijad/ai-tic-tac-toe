2802864b7707ef86739abd4fac3d01f17b04f6fa
DataMonarch <toghrultahirov2@gmail.com>
2023-05-07 15:17:25 +0400

    Latest

--- app/model/board.py
+++ app/model/board.py
@@ -214,7 +214,7 @@ class Board:
         # print(f"player_seq_score: {player_seq_score}, opponent_seq_score: {opponent_seq_score}")
         # score = 10*(player_seq_score - opponent_seq_score) + (player_num_empty_seq - opponent_num_empty_seq) + (win_score_player - win_score_opponent)
         # return score if self.get_mark() == side else -score
-        return 5*(player_seq_score - 3 * opponent_seq_score) + 3*(player_num_empty_seq - 2*opponent_num_empty_seq) + (2*win_score_player - win_score_opponent)
+        return 5*(player_seq_score - 3 * opponent_seq_score) + 3*(player_num_empty_seq - 2*opponent_num_empty_seq) + (win_score_player - win_score_opponent)
     
     
     def evaluate_new(self, depth):
@@ -304,7 +304,7 @@ class Board:
                             
                         if next_seq.count('-') == m-1:
                             opponent_num_empty_seq += 1
-                       
+                    
                         
                 # Check diagonal (top-left to bottom-right)
                 if i <= n - m and j <= n - m:
@@ -312,7 +312,7 @@ class Board:
                     
                     if opponent not in next_seq:
                         count_side = next_seq.count(side)
-                        player_seq_score += count_side**2
+                        player_seq_score += count_side
                         
                         # check if player wins
                         if count_side == m:
@@ -326,7 +326,7 @@ class Board:
                             
                     elif side not in next_seq:
                         opponent_count = next_seq.count(opponent)
-                        opponent_seq_score += opponent_count**2
+                        opponent_seq_score += opponent_count
                         
                         if opponent_count == m:
                             win_score_opponent += win_score - depth
@@ -344,7 +344,7 @@ class Board:
                 
                     if opponent not in next_seq:
                         count_side = next_seq.count(side)
-                        player_seq_score += count_side**2
+                        player_seq_score += count_side
                         
                         # check if player wins
                         if count_side == m:
@@ -358,7 +358,7 @@ class Board:
                             
                     elif side not in next_seq:
                         opponent_count = next_seq.count(opponent)
-                        opponent_seq_score += opponent_count**2
+                        opponent_seq_score += opponent_count
                         
                         if opponent_count == m:
                             win_score_opponent += win_score - depth
@@ -371,7 +371,7 @@ class Board:
             
                             
         # print(f"player_seq_score: {player_seq_score}, opponent_seq_score: {opponent_seq_score}")
-        score = 5*(player_seq_score - 3*opponent_seq_score) + 3*(player_num_empty_seq - 2*opponent_num_empty_seq) + (win_score_player - 2*win_score_opponent)
+        score = 5*(player_seq_score - 3*opponent_seq_score) + 3*(player_num_empty_seq - 2*opponent_num_empty_seq) + (win_score_player - win_score_opponent)
         return score if self.get_mark() == side else -score
         # return 10*(player_seq_score - opponent_seq_score) + (player_num_empty_seq - opponent_num_empty_seq) + (win_score_player - win_score_opponent)
         
