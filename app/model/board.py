c3a31f70878798645bfbcaa42789e05f08218bcc
DataMonarch <toghrultahirov2@gmail.com>
2023-05-07 20:47:53 +0400

    Revert "Latest"
    This reverts commit 4d8e5829c6898cf5a09fc9d65fbe942a922669fc.

--- app/model/board.py
+++ app/model/board.py
@@ -214,7 +214,7 @@ class Board:
         # print(f"player_seq_score: {player_seq_score}, opponent_seq_score: {opponent_seq_score}")
         # score = 10*(player_seq_score - opponent_seq_score) + (player_num_empty_seq - opponent_num_empty_seq) + (win_score_player - win_score_opponent)
         # return score if self.get_mark() == side else -score
-        return 5*(player_seq_score - 3 * opponent_seq_score) + 3*(player_num_empty_seq - 2*opponent_num_empty_seq) + (win_score_player - 2*win_score_opponent)
+        return 5*(player_seq_score - 3 * opponent_seq_score) + 3*(player_num_empty_seq - 2*opponent_num_empty_seq) + (win_score_player - win_score_opponent)
     
     
     def evaluate_new(self, depth):
